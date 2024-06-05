6af0eeb6d0cde5b93d21b095943a8b557a309253
Shafaqsardar2 <shafaqmateen949@gmail.com>
2024-06-05 23:01:56 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const userAns : {
+    sentence : string,
+} = await inquirer.prompt (
+    {
+        type : "input",
+        message : "Enter Your sentence here to count word",
+        name : "sentence",
+    }
+)
+const Words = userAns.sentence.trim().split(" ")
+
+console.log (`Your sentence have ${Words.length} words`)
